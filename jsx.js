1c89df7558bc12524e906b27f6706a25b35835eb
Diriector_Doc <davijust213@yahoo.ca>
2022-05-04 13:25:11 -0400

    Usable for void elements

--- jsx.js
+++ jsx.js
@@ -1,18 +1,25 @@
 "use strict";
 globalThis.jsx = (() => {
 
-	const searchHTML = /^<(?<tag>[a-z][a-z0-9\-]*)\s*(?<attributes>(\s+([a-z][a-z0-9\-]*)(\s*=\s*"([^"]*)")?)*)\s*>(?<content>.*)<\/\1(\s.*)?>$/i,
+	const searchVoid = /^<(?<tag>area|base|br|col|command|embed|hr|img|input|keygen|link|meta|param|source|track|wbr)\s*(?<attributes>(\s+([a-z][a-z0-9\-]*)(\s*=\s*"([^"]*)")?)*)\s*\/?>$/i,
+		searchHTML = /^<(?<tag>[a-z][a-z0-9\-]*)\s*(?<attributes>(\s+([a-z][a-z0-9\-]*)(\s*=\s*"([^"]*)")?)*)\s*>(?<content>.*)<\/\1(\s.*)?>$/i,
 		splitAttribute = /([a-z][a-z0-9\-]*)(\s*=\s*"[^"]*")?/i,
 		equalsQuotes = /\s*=\s*"([^"]*)".*/;
 
-	return (str) => {
+	return function(str){
 		if(Array.isArray(str)){
-			str = str[0]
+			str = str[0];
+			for(let i = 1; i < arguments.length; i++){
+				str += arguments[i] instanceof Element ? arguments[i].outerHTML : arguments[i];
+				str += arguments[0][i];
+			}
 		}
-		let foundElement = searchHTML.exec(str.trim());
+		let foundElement = searchVoid.exec(str.trim()) || searchHTML.exec(str);
 		if(foundElement?.groups){
 			let elem = document.createElement(foundElement.groups.tag);
-			elem.innerHTML = foundElement.groups.content;
+			if(foundElement.groups.content){
+				elem.innerHTML = foundElement.groups.content;
+			}
 			if(foundElement.groups.attributes){
 				let arr = foundElement.groups.attributes.split(splitAttribute).filter(e => e === undefined || e.trim());
 				for(let i = 0; i < arr.length; i += 2){
