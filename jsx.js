67c95d9c9a37a20acaeec3ae1a2ca796c758a745
Diriector_Doc <davijust213@yahoo.ca>
2022-06-08 16:33:56 -0400

    Works when element has no siblings.

--- jsx.js
+++ jsx.js
@@ -1,13 +1,27 @@
 "use strict";
 globalThis.jsx = function(str){
+	let elem = document.createElement("div");
 	if(Array.isArray(str)){
 		str = str[0];
 		for(let i = 1; i < arguments.length; i++){
-			str += arguments[i] instanceof Element ? arguments[i].outerHTML : arguments[i];
+			str += arguments[i] instanceof Element ? `<br jsx-replace-index="${i}">` : arguments[i];
 			str += arguments[0][i];
 		}
+		elem.innerHTML = str;
+		for(let i = 1; i < arguments.length; i++){
+			if(arguments[i] instanceof Element){
+				let parent = elem.querySelector(`br[jsx-replace-index="${i}"]`).parentElement,
+					content = parent.innerHTML.split(`<br jsx-replace-index="${i}">`).map(e => {
+						let tpl = document.createElement("template");
+						tpl.innerHTML = e;
+						return tpl.content
+					});
+				parent.innerHTML = "";
+				parent.append(content[0], arguments[i], content[1])
+			}
+		}
+	} else {
+		elem.innerHTML = str;
 	}
-	let div = document.createElement("div");
-	div.innerHTML = str;
-	return div.children[0]
+	return elem.children[0];
 }
