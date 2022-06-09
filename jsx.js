e0a614be6e51db6aa6aaa5281690a1dd4307a638
Diriector_Doc <davijust213@yahoo.ca>
2022-06-09 13:20:24 -0400

    This works

--- jsx.js
+++ jsx.js
@@ -1,24 +1,20 @@
 "use strict";
 globalThis.jsx = function(str){
-	let elem = document.createElement("div");
+	let elem = document.createElement("div"),
+		indexes = [];
 	if(Array.isArray(str)){
 		str = str[0];
 		for(let i = 1; i < arguments.length; i++){
-			str += arguments[i] instanceof Element ? `<br jsx-replace-index="${i}">` : arguments[i];
+			str += arguments[i] instanceof Element ? (indexes.push(i), `<br jsx-replace-index="${i}">`) : arguments[i];
 			str += arguments[0][i];
 		}
 		elem.innerHTML = str;
-		for(let i = 1; i < arguments.length; i++){
-			if(arguments[i] instanceof Element){
-				let parent = elem.querySelector(`br[jsx-replace-index="${i}"]`).parentElement,
-					content = parent.innerHTML.split(`<br jsx-replace-index="${i}">`).map(e => {
-						let tpl = document.createElement("template");
-						tpl.innerHTML = e;
-						return tpl.content
-					});
-				parent.innerHTML = "";
-				parent.append(content[0], arguments[i], content[1])
-			}
+		for(let i of indexes){
+			let parent = elem.querySelector(`br[jsx-replace-index="${i}"]`).parentElement,
+				children = [...parent.childNodes];
+			children[children.findIndex(e => e.matches(`br[jsx-replace-index="${i}"]`))] = arguments[i]
+			parent.innerHTML = "";
+			parent.append(...children)
 		}
 	} else {
 		elem.innerHTML = str;
