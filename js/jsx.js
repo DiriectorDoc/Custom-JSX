13672d22a1aedd6abee29683ec5340b115b29a8f
Diriector_Doc <davijust213@yahoo.ca>
2023-03-15 11:49:56 -0400

    Last time broke it. This fixed it.

--- js/jsx.js
+++ js/jsx.js
@@ -1,11 +1,18 @@
 "use strict";
 globalThis.jsx = function(str){
-	var elem = document.createElement("template");
+	let elem = document.createElement("template"), indexes = [];
 	if (Array.isArray(str)) {
-		elem.append(arguments[0][0]);
-		for (var i = 1; i < arguments.length; i++) {
-			elem.append(arguments[i] instanceof Node ? arguments[i] : "".concat(arguments[i]));
-			elem.append(arguments[0][i]);
+		str = str[0];
+		for (let i = 1; i < arguments.length; i++) {
+			str += arguments[i] instanceof Node ? (indexes.push(i), `<br jsx-replace-index="${i}">`) : arguments[i];
+			str += arguments[0][i];
+		}
+		elem.innerHTML = str;
+		for (let i of indexes) {
+			let parent = elem.querySelector(`br[jsx-replace-index="${i}"]`).parentElement, children = [...parent.childNodes];
+			children[children.findIndex(e => e.matches(`br[jsx-replace-index="${i}"]`))] = arguments[i];
+			parent.innerHTML = "";
+			parent.append(...children);
 		}
 	}
 	else {
